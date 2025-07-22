7ba9d3ddaaa1c6b7b5dee38a726de391d60417eb
Logan <pix.59700@gmail.com>
2025-07-22 16:04:05 +0200

    Player movement

---
+++ CDVEL-Game/Assets/_Core/Scripts/Players/Mouvements/PlayerMovementCommand.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Players.Mouvements
+{
+    using Commands;
+    
+    public class PlayerMovementCommand : ICommand
+    {
+        private GameObject _gameObject;
+        private CharacterController _controller;
+        private Vector3 _direction;
+        private float _speed;
+        
+        public PlayerMovementCommand(GameObject gameObject, 
+                                    CharacterController controller, 
+                                    Vector3 direction, 
+                                    float speed)
+        {
+            _gameObject = gameObject;
+            _controller = controller;
+            _direction = direction;
+            _speed = speed;
+        }
+        
+        public void Execute()
+        {
+            Vector3 currentMovement = _gameObject.transform.right * (_direction.x * _speed)
+                                        + _gameObject.transform.up * _direction.y;
+            
+            _controller.Move(currentMovement * Time.fixedDeltaTime);
+        }
+
+        public void Undo()
+        {
+            Vector3 currentMovement = _gameObject.transform.right * (_direction.x * _speed)
+                                      + _gameObject.transform.up * _direction.y;
+            
+            _controller.Move(-currentMovement * Time.fixedDeltaTime);
+        }
+    }
+}
---
+++ CDVEL-Game/Assets/_Core/Scripts/Players/Mouvements/PlayerMovementInvoker.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine.InputSystem;
+using UnityEngine;
+
+namespace Players.Mouvements
+{
+    using Extensions;
+    using Commands;
+    
+    [RequireComponent(typeof(CharacterController), typeof(Collider))]
+    public class PlayerMovementInvoker : CommandInvoker
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private float _gravityDetectionDistance;
+
+        private Vector3 _direction;
+        private CharacterController _controller;
+
+        private void Awake()
+        {
+            _controller = GetComponent<CharacterController>();
+        }
+
+        private void FixedUpdate()
+        {
+            ExecuteMovement();
+        }
+
+        public void SetDirection(InputAction.CallbackContext ctx) => _direction = new Vector3(ctx.ReadValue<Vector2>().x, 0, 0);
+
+        private void ExecuteMovement()
+        {
+            _direction.y = gameObject.ApplyGravity(_direction.y, _gravityDetectionDistance);
+
+            if (_direction == Vector3.zero)
+                return;
+            
+            PlayerMovementCommand newCommand = new PlayerMovementCommand(gameObject, _controller, _direction, _speed);
+            ExecuteCommand(newCommand);
+        }
+    }
+}
