5bdb3ccae63fcc5db87728cb7bb2a0671f5d5cf7
Logan <pix.59700@gmail.com>
2025-07-26 19:38:55 +0200

    Heart spawner

---
+++ CDVEL-Game/Assets/_Core/Scripts/Spawner/HeartSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Spawner
+{
+    public class HeartSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] _heartPrefab;
+        [SerializeField] private Transform[] _spawnPoints;
+        
+        [SerializeField] private float _minHeartSpawnRate;
+        [SerializeField] private float _maxHeartSpawnRate;
+
+        private void Start()
+        {
+            CreateHeart();
+        }
+
+        private void CreateHeart()
+        {
+            IEnumerator SpawnHeart()
+            {
+                GameObject instance = _heartPrefab[Random.Range(0, _heartPrefab.Length)];
+                Transform position = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+                
+                yield return new WaitForSeconds(Random.Range(_minHeartSpawnRate, _maxHeartSpawnRate));
+                
+                Instantiate(instance, position.position, Quaternion.identity);
+
+                StartCoroutine(SpawnHeart());
+            }
+            
+            StartCoroutine(SpawnHeart());
+        }
+    }
+}
