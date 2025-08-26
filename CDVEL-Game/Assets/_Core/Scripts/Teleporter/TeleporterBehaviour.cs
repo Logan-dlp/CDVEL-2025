1519a0fcd8844a27b036c2da171ab42eb852d6f3
Logan <pix.59700@gmail.com>
2025-08-26 16:19:54 +0200

    Adding teleporter

---
+++ CDVEL-Game/Assets/_Core/Scripts/Teleporter/TeleporterBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Teleporter
+{
+    [RequireComponent(typeof(Collider))]
+    public class TeleporterBehaviour : MonoBehaviour
+    {
+        [SerializeField] private LayerMask _teleportableMask;
+        
+        private Transform _arrivedTransform;
+        private ITeleporterSystem _teleporterSystem;
+
+        private void Awake()
+        {
+            if (transform.childCount == 0)
+            {
+                Debug.LogWarning("TeleporterBehaviour needs at least one arrived transform");
+                return;
+            }
+            
+            _arrivedTransform = transform.GetChild(0);
+
+            _teleporterSystem = new TeleporterSystem(_arrivedTransform);
+        }
+
+        private void OnTriggerEnter(Collider collision)
+        {
+            if (_teleportableMask == (_teleportableMask | (1 << collision.gameObject.layer)) && _teleporterSystem != null)
+            {
+                _teleporterSystem.Teleport(collision.gameObject);
+            }
+        }
+    }
+}
