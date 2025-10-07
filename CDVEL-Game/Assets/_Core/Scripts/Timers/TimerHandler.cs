5f2bb9a711cf72b68f91afcf4002cfddabd0d70e
Logan <pix.59700@gmail.com>
2025-10-07 22:29:10 +0200

    Make Singleton Timer

---
+++ CDVEL-Game/Assets/_Core/Scripts/Timers/TimerHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace Timers
+{
+    using Singletons;
+    
+    public class TimerHandler : MonoSingleton<TimerHandler>
+    {
+        public event Action OnStartedTimer;
+        public event Action OnStoppedTimer;
+        
+        public event Action<float> OnTimerUpdate; 
+        
+        [SerializeField] private float _timer;
+        
+        private ITimerSystem _timerSystem;
+
+        private void Update()
+        {
+            _timerSystem.UpdateTimer();
+        }
+
+        protected override void OnInitializing()
+        {
+            base.OnInitializing();
+            _timerSystem = new TimerSystem(_timer);
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            OnStartedTimer += _timerSystem.StartTimer;
+            _timerSystem.OnTimerUpdate += OnTimerUpdate;
+            OnStoppedTimer += _timerSystem.StopTimer;
+        }
+
+        public override void Uninitialize()
+        {
+            base.Uninitialize();
+            OnStartedTimer -= _timerSystem.StartTimer;
+            _timerSystem.OnTimerUpdate -= OnTimerUpdate;
+            OnStoppedTimer -= _timerSystem.StopTimer;
+        }
+    }
+}
---
+++ CDVEL-Game/Assets/_Core/Scripts/Timers/TimerSystem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+namespace Timers
+{
+    public class TimerSystem : ITimerSystem
+    {
+        public event Action OnTimerFinished;
+        public event Action<float> OnTimerUpdate;
+        
+        private float _currentTimer;
+        public float CurrentTimer => _currentTimer;
+
+        private float _timer;
+        public float Timer => _timer;
+        
+        private bool _isRunning;
+
+        public TimerSystem(float timer)
+        {
+            _timer = timer;
+            ResetTimer();
+        }
+        
+        public void StartTimer()
+        {
+            _isRunning = true;
+        }
+
+        public void UpdateTimer()
+        {
+            if (_isRunning)
+            {
+                _timer -= Time.deltaTime;
+                OnTimerUpdate?.Invoke(_timer);
+                if (_timer <= 0)
+                {
+                    StopTimer();
+                    OnTimerFinished?.Invoke();
+                    StopTimer();
+                }
+            }
+        }
+
+        public void StopTimer()
+        {
+            _isRunning = false;
+        }
+
+        public void ResetTimer()
+        {
+            _currentTimer = _timer;
+        }
+    }
+}
