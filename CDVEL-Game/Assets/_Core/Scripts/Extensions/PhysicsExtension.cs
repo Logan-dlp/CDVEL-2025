dea550b298f50217c80091338a4faa91f701c47a
Logan <pix.59700@gmail.com>
2025-10-10 15:16:57 +0200

    make jump feature for player

--- CDVEL-Game/Assets/_Core/Scripts/Extensions/PhysicsExtension.cs
+++ CDVEL-Game/Assets/_Core/Scripts/Extensions/PhysicsExtension.cs
@@ -2,13 +2,19 @@
 
 namespace Extensions
 {
-    public static class GameObjectExtension
+    public static class PhysicsExtension
     {
         public static float ApplyGravity(this GameObject gameObject, float currentGravity, float distance)
         {
             return Physics.Raycast(gameObject.transform.position, -gameObject.transform.up, out RaycastHit hit, distance)
                    && hit.transform != gameObject.transform
                 ? 0 : currentGravity + Physics.gravity.y * Time.fixedDeltaTime;
+        }
+
+        public static bool IsGrounded(this GameObject gameObject, float distance)
+        {
+            return Physics.Raycast(gameObject.transform.position, -gameObject.transform.up, out RaycastHit hit,
+                       distance) && hit.transform != gameObject.transform;
         }
     }
 }
