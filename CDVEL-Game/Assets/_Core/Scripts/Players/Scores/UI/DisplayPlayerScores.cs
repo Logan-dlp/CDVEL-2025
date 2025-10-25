8923d34f4e6a788fbaf8eb05961e6e79959b9b22
Logan <pix.59700@gmail.com>
2025-10-25 23:01:11 +0200

    Make winned event

--- CDVEL-Game/Assets/_Core/Scripts/Players/Scores/UI/DisplayPlayerScores.cs
+++ CDVEL-Game/Assets/_Core/Scripts/Players/Scores/UI/DisplayPlayerScores.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Events;
 
 namespace Players
 {
@@ -8,6 +11,8 @@ namespace Players
     public class DisplayPlayerScores : MonoBehaviour, IDisplay
     {
         [SerializeField] private PlayerTag _playerTag;
+        [SerializeField] private UnityEvent _OnGameWined;
+        [SerializeField] private UnityEvent _OnGameOver;
         
         private TextMeshProUGUI _text;
 
@@ -21,6 +26,44 @@ namespace Players
         {
             int score = PlayerPrefs.GetInt(_playerTag.ToString(), 0);
             _text.text = score.ToString();
+            
+            InvokeCallbacks(score, _playerTag);
+        }
+
+        private void InvokeCallbacks(int score, PlayerTag tag)
+        {
+            bool IsWinner(int score, PlayerTag tag)
+            {
+                var scores = new int[Enum.GetValues(typeof(PlayerTag)).Length - 2];
+                // Starts with a 1 because the first one is "None".
+                int j = 0;
+                for (int i = 1; i < Enum.GetValues(typeof(PlayerTag)).Length; i++)
+                {
+                    if ((PlayerTag)i != tag)
+                    {
+                        scores[j] = PlayerPrefs.GetInt(((PlayerTag)i).ToString());
+                        ++j;
+                    }
+                }
+
+                bool isWinner = true;
+
+                foreach (int opponentScores in scores)
+                {
+                    if (opponentScores > score)
+                    {
+                        isWinner = false;
+                        break;
+                    }
+                }
+
+                return isWinner;
+            }
+
+            if (IsWinner(score, tag))
+                _OnGameWined?.Invoke();
+            else
+                _OnGameOver?.Invoke();
         }
     }
 }
