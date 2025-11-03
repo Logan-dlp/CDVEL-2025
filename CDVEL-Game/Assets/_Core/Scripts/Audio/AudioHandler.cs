f47f934ec6cd13d6751764309d9e278142edf6c1
FTFumetsu <esmieudamien18@gmail.com>
2025-11-03 15:09:40 +0100

    Modify writing conventions and small issues

--- CDVEL-Game/Assets/_Core/Scripts/Audio/AudioHandler.cs
+++ CDVEL-Game/Assets/_Core/Scripts/Audio/AudioHandler.cs
@@ -1,5 +1,4 @@
 using Singletons;
-using System.Net.Http.Headers;
 using UnityEngine;
 
 namespace Audio
@@ -10,9 +9,6 @@ namespace Audio
         private AudioSource _audioSource;
 
         private IAudioSystem _audioSystem;
-        private AudioHandler _instance;
-
-        public AudioHandler HandlerInstance => _instance;
 
         protected override void Awake()
         {
@@ -20,27 +16,15 @@ namespace Audio
             _audioSource = GetComponent<AudioSource>();
 
             _audioSystem = new AudioSystem();
-            _instance = this;
         }
 
         public void PlaySound(SoundData data)
         {
-            if(_audioSystem == null)
-            {
-                Debug.LogWarning("AudioSystem is null so can't play the sound");
-            }
-
-            if(data == null)
-            {
-                Debug.LogWarning("SounData is null");
-            }
-
             _audioSystem.Play(data, _audioSource);
         }
 
         public void StopSound()
         {
-            if (_audioSystem == null) return;
             _audioSystem.Stop(_audioSource);
         }
     }
