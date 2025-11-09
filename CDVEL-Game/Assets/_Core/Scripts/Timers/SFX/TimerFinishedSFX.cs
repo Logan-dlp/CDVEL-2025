2b2f4ae01d44b67520ada21eb4bb8e997ca32ddc
FTFumetsu <esmieudamien18@gmail.com>
2025-11-09 20:12:21 +0100

    Add victory sound

--- CDVEL-Game/Assets/_Core/Scripts/Timers/SFX/TimerFinishedSFX.cs
+++ CDVEL-Game/Assets/_Core/Scripts/Timers/SFX/TimerFinishedSFX.cs
@@ -5,22 +5,18 @@ using UnityEngine;
 public class TimerFinishedSFX : SFXLauncher
 {
     [SerializeField] private SoundData _finalCountdownSound;
-    [SerializeField] private SoundData _victorySound;
 
     protected override void Awake()
     {
         base.Awake();
         if (_fx == null) _fx = _finalCountdownSound;
         TimerHandler.Instance.OnFinalCountdownLauch += PlayFX;
-        _fx = _victorySound;
         TimerHandler.Instance.OnTimerFinished += StopSFX;
-        TimerHandler.Instance.OnTimerFinished += PlayFX;
     }
 
     private void OnDestroy()
     {
         TimerHandler.Instance.OnFinalCountdownLauch -= PlayFX;        
         TimerHandler.Instance.OnTimerFinished -= StopSFX;
-        TimerHandler.Instance.OnTimerFinished -= PlayFX;
     }
 }
